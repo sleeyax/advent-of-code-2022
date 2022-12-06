f22dd0c964be3ff2adb1825498e00804803db463
Sleeyax <yourd3veloper@gmail.com>
2022-12-07 00:19:28 +0100

    Day 6: solve part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,23 +1,25 @@
 use itertools::Itertools;
 
-const MARKER_LENGTH: usize = 4;
-
-pub fn part_one(input: &str) -> Option<u32> {
-    let line = input.lines().next().unwrap();
-
+fn find_marker(line: &str, marker_length: usize) -> Option<u32> {
     for (i, _)  in line.chars().enumerate() {
         let n = i+1;
-        let marker = line.chars().skip(n).take(MARKER_LENGTH);
-        if marker.sorted().dedup().count() == MARKER_LENGTH {
-            return Some((n + MARKER_LENGTH) as u32);
+        let marker = line.chars().skip(n).take(marker_length);
+        if marker.sorted().dedup().count() == marker_length {
+            return Some((n + marker_length) as u32);
         }
     }
 
     None
 }
 
+pub fn part_one(input: &str) -> Option<u32> {
+    let line = input.lines().next().unwrap();
+    find_marker(line, 4)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let line = input.lines().next().unwrap();
+    find_marker(line, 14)
 }
 
 fn main() {
@@ -39,6 +41,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 6);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(19));
     }
 }
