a1bf272d8cf53f46a600c0f73f5d7d71a1d6433e
Sleeyax <yourd3veloper@gmail.com>
2022-12-11 22:29:23 +0100

    Day 8: solve part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -13,22 +13,39 @@ fn is_visible(current: &u32, range: Vec<u32>) -> bool {
     range.iter().all(|v| v < current)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut count = 0;
+fn get_distance(current: &u32, range: Vec<u32>) -> u32 {
+    let mut distance = 0;
+
+    for i in range {
+        if i < *current {
+            distance += 1;
+        } else if i >= *current {
+            distance += 1;
+            break;
+        }
+    }
+
+    distance
+}
+
+fn process(input: &str, part_two: bool) -> u32 {
+    let mut total = 0;
     let grid = parse_grid(input);
 
     for (r, row) in grid.iter().enumerate() {
         for (c, column) in row.iter().enumerate() {
             // edges are always visible
             if r == 0 || c == 0 || r == row.len() - 1 || c == grid.len() - 1 {
-                count += 1;
+                if !part_two {
+                    total += 1;
+                }
                 continue;
             }
 
-            let left = row[0..c].to_vec();
+            let mut left = row[0..c].to_vec();
             let right = row[c + 1..row.len()].to_vec();
-            let top = grid.iter().take(r).map(|vec| vec[c]).collect::<Vec<_>>();
-            let bottom = grid
+            let mut up = grid.iter().take(r).map(|vec| vec[c]).collect::<Vec<_>>();
+            let down = grid
                 .iter()
                 .skip(r + 1)
                 .map(|vec| vec[c])
@@ -36,21 +53,39 @@ pub fn part_one(input: &str) -> Option<u32> {
 
             // println!("{:?} {} {:?}", left, column, right);
 
-            if is_visible(column, left)
-                || is_visible(column, right)
-                || is_visible(column, top)
-                || is_visible(column, bottom)
-            {
-                count += 1;
+            if !part_two {
+                if is_visible(column, left)
+                    || is_visible(column, right)
+                    || is_visible(column, up)
+                    || is_visible(column, down)
+                {
+                    total += 1;
+                }
+            } else {
+                up.reverse();
+                left.reverse();
+                let scenic_score = get_distance(column, up)
+                    * get_distance(column, left)
+                    * get_distance(column, right)
+                    * get_distance(column, down);
+                if scenic_score > total {
+                    total = scenic_score;
+                }
             }
         }
     }
 
+    total
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let count = process(input, false);
     Some(count)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let count = process(input, true);
+    Some(count)
 }
 
 fn main() {
@@ -83,6 +118,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 8);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(8));
     }
 }
